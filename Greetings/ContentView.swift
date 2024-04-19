f4d0f8231a3fdaac874eaf0c3861ecf8d7a01dce
Jai <jai07rajput@gmail.com>
2024-04-20 00:19:28 +0530

    DataModel

--- Greetings/ContentView.swift
+++ Greetings/ContentView.swift
@@ -8,13 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let messages:[DataItemModel] = [
+    DataItemModel(text: "Hey", color: .green),
+    DataItemModel(text: "Welcome to The Club", color: .gray),
+    DataItemModel(text: "Are You Ready", color: .yellow),
+    DataItemModel(text: "Start Exploring", color: .red),
+    DataItemModel(text: "Boom", color: .purple)
+
+    ]
     var body: some View {
         VStack(alignment: .leading) {
-            TextView(text: "Hey", color: .green)
-            TextView(text: "Welcome to The Club", color: .gray)
-            TextView(text: "Are You Ready", color: .yellow)
-            TextView(text: "Start Exploring", color: .red)
-            TextView(text: "Boom", color: .purple)
+            TextView(text: messages[0].text, color: messages[0].color)
+            TextView(text: messages[1].text, color: messages[1].color)
+            TextView(text: messages[2].text, color: messages[2].color)
+            TextView(text: messages[3].text, color: messages[3].color)
+            TextView(text: messages[4].text, color: messages[4].color)
             
             
         }
---
+++ Greetings/DataItemmodel.swift
@@ -0,0 +1,14 @@
+//
+//  DataItemmodel.swift
+//  Greetings
+//
+//  Created by Jai  on 20/04/24.
+//
+
+import Foundation
+import SwiftUI
+struct DataItemModel{
+    let text:String
+    let color:Color
+}
+
