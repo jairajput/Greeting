d935444cfb11be122f6ed1069e595f7e71f4c934
Jai <jai07rajput@gmail.com>
2024-04-20 11:31:04 +0530

    Added-Gradient

---
+++ Greetings/BackgroundView.swift
@@ -0,0 +1,21 @@
+//
+//  BackgroundView.swift
+//  Greetings
+//
+//  Created by Jai  on 20/04/24.
+//
+
+import SwiftUI
+
+struct BackgroundView: View {
+    var body: some View {
+        LinearGradient(colors: [.blue,Color(red:139/255,green: 80/255,blue: 240/255)], startPoint: .topLeading, endPoint: .bottomTrailing)
+            .opacity(0.3)
+            .ignoresSafeArea()
+
+    }
+}
+
+#Preview {
+    BackgroundView()
+}
--- Greetings/ContentView.swift
+++ Greetings/ContentView.swift
@@ -11,8 +11,7 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            Color.black.opacity(0.2)
-                .ignoresSafeArea() 
+            BackgroundView()
             VStack(alignment: .leading) {
                 //Title
                 TitleView()
